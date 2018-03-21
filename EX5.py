776bb8bdb08f71e3d2a9961d9920567b22f23eab
LiuQiang <liuqiang@zhongjiaxin.com>
2018-03-21 15:08:12 +0800

    3.21

---
+++ EX5.py
@@ -0,0 +1,18 @@
+
+name = "aba lovelace"
+
+#将每个单词的首字母都改为大写
+print(name.title())
+
+#将字符串先转换为大写
+print(name.upper())
+
+#将字符串先转换为小写
+print(name.lower())
+
+#合并（拼接）字符串
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+massage = "Hello, "  +full_name.title() + "!"
+print(massage)
---
+++ EX7.py
@@ -0,0 +1,27 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'gary', 'york']
+
+first_owned = motorcycles.pop(0)
+
+print(motorcycles)
+print(first_owned)
+
+print('The fist motorcucle I owned was a '  + first_owned.title() + '.')
+
+#让Python确定'ducati'出现在列表的什么地方，并将该元素删除
+motorcycles.remove('yamaha')
+
+print(motorcycles)
+
+too_expensive = 'gary'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
+
+
+#使用方法 sort()对列表进行永久性排序
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort()
+print(cars)
+#向sort()方法传递参数 reverse=True,实现倒序
+cars.sort(reverse=True)
+print(cars)
