961e50b5f2978120f38c4683a89513483a123d35
liuqiang <stra130822@163.com>
2018-06-15 15:33:57 +0800

    6.15

---
+++ EX18.py
@@ -0,0 +1,25 @@
+print("Hello Word!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+
+
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print("I could have code like this.")  # and the comment after is ignored
+# You can also use a comment to "disable" or comment out a piece of code :
+# print "This won't run."
+print("This will run.")
+
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters" , 100 - 25 * 3 % 4)
+print("Now I will count the eggs:")
+
