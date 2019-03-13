f1c578806a5361a2caffaa2f03bb9da291a1bd39
liuqiang <stra130822@163.com>
2019-03-13 10:27:27 +0800

    Default

--- EX18.py
+++ EX18.py
@@ -1,25 +1,34 @@
-print("Hello Word!")
-print("Hello Again")
-print("I like typing this.")
-print("This is fun.")
-print("Yay! Printing.")
-print("I'd much rather you 'not'.")
-print('I "said" do not touch this.')
+# 函数的参数:
+#
+# ( )表示调用, 传参
+#
+# 参数:参数是给函数传递的信息. 分为实参和形参
+#
+# 注意:在函数调用的时候,必须保证所有的形参都有值
+#
+# 形参:在函数声明的位置写的变量
+def regist(name, age, edu, gender="男"):
+    print("name:",name)
+    print("age:",age)
+    print("edu:",edu)
+    print("gender:",gender)
+regist('gary', 20, '本科')
 
 
+#实参:在函数调用的时候给函数传递的具体的值
+def chi(zhushi,fushi,tang,tiandian):
+    print('主食：%s' % zhushi)
+    print('副食：%s' % fushi)
+    print('汤：%s' % tang)
+    print('甜点：%s' % tiandian)
+#1.位置参数：按照位置给形参传递信息
+chi('牛肉板面','豆皮','鸡蛋汤','蛋糕')
 
+# 2.关键字参数: 按照形参声明的变量名进行传参
+chi(zhushi='牛肉板面',fushi='豆皮',tang='鸡蛋汤',tiandian='冰激凌')
 
-# A comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-print("I could have code like this.")  # and the comment after is ignored
-# You can also use a comment to "disable" or comment out a piece of code :
-# print "This won't run."
-print("This will run.")
+# 3.混合参数: 位置 + 关键字(规定:关键字必须在最后)
+# chi(tang='鸡蛋汤',tiandian='冰激凌', '牛肉板面', '豆皮', )   # 报错
 
-
-print("I will now count my chickens:")
-
-print("Hens", 25 + 30 / 6)
-print("Roosters" , 100 - 25 * 3 % 4)
-print("Now I will count the eggs:")
-
+chi('牛肉板面', '豆皮', tang='鸡蛋汤',tiandian='冰激凌')
+chi('牛肉板面', '豆皮', tiandian='冰激凌',tang='鸡蛋汤')
---
+++ EX31.py
@@ -0,0 +1,19 @@
+"""
+ File Name: EX31.py
+ Version  : 1.0
+ Author   : Gary
+ Date     : 2018/12/6 16:17
+ Software : PyCharm
+ 
+"""
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+
+url = 'http://www.baidu.com'
+
+driver.get(url)
+
+driver.close()
+
+#driver.quit()
