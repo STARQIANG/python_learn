fb05b0ba38f5a32a1b3d12802778749fb1fba22e
liuqiang <stra130822@163.com>
2019-10-21 16:17:39 +0800

    Default

--- archiver_order.sh
+++ archiver_order.sh
@@ -9,10 +9,10 @@ SLAVE03=bcex-read-003.cnhbjn4dkymy.ap-northeast-2.rds.amazonaws.com
 SLAVE04=bcex-read-004.cnhbjn4dkymy.ap-northeast-2.rds.amazonaws.com
 
 LAST_DAY=30
-
 UP_DATA=`date +"%Y-%m-%d 00:00:00" -d "${LAST_DAY} days ago"`
 
-
+API_LAST_DAY=1
+API_UP_DATA=`date +"%Y-%m-%d 00:00:00" -d "${API_LAST_DAY} days ago"`
 # pt-archiver路径
 
 PTAR="/usr/bin/pt-archiver"
@@ -39,6 +39,15 @@ if [ ! -d ${LOG_PATH} ];then
     mkdir ${LOG_PATH} -p
 fi
 
+LOG_FILE="${LOG_PATH}/order_history.log"
+API_LOG_FILE="${LOG_PATH}/api_order_history.log"
+
+COUNT_LOG="${LOG_PATH}/archiver_${SOURCE_TAB}.log"
+
+#所有用户归档30天前订单；
+
+function ALL_END_ORDER() {
+echo "#################################所有用户${LAST_DAY}天前订单开始归档#################################" | tee -a ${COUNT_LOG}
 for((i=0;i<32;i++));
 do
     NOW_TIME=`date +"%F %T"`
@@ -50,17 +59,14 @@ do
 
     #SQL_CMD= " updated_at < ${UP_DATA} AND org_id = 10 AND user_id MOD 32 = ${i} AND status IN (0,3) "
 
-    LOG_FILE="${LOG_PATH}/${DEST_TAB}-${DAY_TIME}.log"
-
     ${PTAR} --source ${MYSQL_S_CMD} --dest ${MYSQL_D_CMD} --no-check-charset --no-check-columns --no-version-check \
             --where " updated_at < '${UP_DATA}' AND org_id = 10 AND user_id MOD 32 = ${i} AND status IN (0,3) " --progress 100000 --limit 1000 --commit-each  \
             --bulk-insert --ignore --low-priority-insert --bulk-delete --statistics --no-delete --max-lag=1  \
-            --check-slave-lag ${SLAVE_LAG_01} --check-slave-lag ${SLAVE_LAG_02} --why-quit | tee ${LOG_FILE}
+            --check-slave-lag ${SLAVE_LAG_01} --check-slave-lag ${SLAVE_LAG_02} --why-quit | tee -a ${LOG_FILE}
 
     if [ $? -eq 0 ]; then
 
-    COUNT_LOG="${LOG_PATH}/archiver_${SOURCE_TAB}.log"
-    INSTER_COUNT=`grep "SELECT" ${LOG_FILE} | tail -n1 |awk '{print $2}'`
+    INSTER_COUNT=`grep "INSTER" ${LOG_FILE} | tail -n1 |awk '{print $2}'`
 
     COUNT=`expr ${INSTER_COUNT} + 0`
 
@@ -70,4 +76,48 @@ do
     fi
 
 done
-echo "#################################本次导入完成#################################" | tee -a ${COUNT_LOG}
+
+echo "#################################所有用户${LAST_DAY}天前订单本次归档完成#################################" | tee -a ${COUNT_LOG}
+
+}
+
+#API用户归档1天前订单
+function API_END_ORDER() {
+
+echo "#################################API用户${API_LAST_DAY}天前订单开始归档#################################" | tee -a ${COUNT_LOG}
+
+for((i=0;i<32;i++));
+do
+    NOW_TIME=`date +"%F %T"`
+
+    DEST_TAB=order_history_${i}
+
+    MYSQL_S_CMD="h=${MYSQL_S_HOST},P=3306,u=root,p=${MYSQL_S_PASSWD},D=bcex,t=${SOURCE_TAB}"
+    MYSQL_D_CMD="h=${MYSQL_D_HOST},P=3306,u=root,p=${MYSQL_D_PASSWD},D=bcex,t=${DEST_TAB}"
+
+    USER_ID=`mysql -uroot -p${MYSQL_S_PASSWD} -h${MYSQL_S_HOST} bcex -N -e " SELECT GROUP_CONCAT(DISTINCT user_id) FROM api_market_apply WHERE status = 1 AND user_id MOD 32 = ${i}"`
+
+    ${PTAR} --source ${MYSQL_S_CMD} --dest ${MYSQL_D_CMD} --no-check-charset --no-check-columns --no-version-check \
+            --where " updated_at < '${API_UP_DATA}' AND org_id = 10 AND user_id IN ( ${USER_ID} ) AND status IN (0,3) " --progress 100000 --limit 1000 --commit-each  \
+            --bulk-insert --ignore --low-priority-insert --bulk-delete --statistics --no-delete --max-lag=1  \
+            --check-slave-lag ${SLAVE_LAG_01} --check-slave-lag ${SLAVE_LAG_02} --why-quit | tee -a ${API_LOG_FILE}
+
+
+    if [ $? -eq 0 ]; then
+
+    INSTER_COUNT=`grep "INSTER" ${API_LOG_FILE} | tail -n1 |awk '{print $2}'`
+
+    COUNT=`expr ${INSTER_COUNT} + 0`
+
+    echo -e "[${NOW_TIME}] [INFO] ${SOURCE_TAB}表API用户updated_at < ${UP_DATA}的数据导入${DEST_TAB}表${COUNT}条！" | tee -a ${COUNT_LOG}
+
+    fi
+done
+
+echo "#################################API用户${API_LAST_DAY}天前订单归档完成#################################" | tee -a ${COUNT_LOG}
+
+}
+
+ALL_END_ORDER
+
+API_END_ORDER
